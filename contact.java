41be72b63f4a5d9d3c65b1ef4d024d82625e2561
Serigne fallou Mbacke <2722471@collegelacite.ca>
2025-04-14 11:39:48 -0400

    Update contact.java

--- contact.java
+++ contact.java
@@ -1,4 +1,5 @@
 import java.io.Serializable;
+import java.util.regex.Pattern;
 public class Contact implements Serializable {
     // Attributs privés représentant les informations du contact
     private String nom;
@@ -7,7 +8,7 @@ public class Contact implements Serializable {
     private String email;
     private String categorie;
 
-  // Constructeur pour initialiser les attributs du contact
+    // Constructeur pour initialiser les attributs du contact
     public Contact(String nom, String prenom, String telephone, String email, String categorie) {
         this.nom = nom;
         this.prenom = prenom;
@@ -15,18 +16,22 @@ public class Contact implements Serializable {
         this.email = email;
         this.categorie = categorie; }
 
-  // Méthodes getters pour accéder aux valeurs des attributs
+    private boolean EmailValide(String email)
+    {String regex = "^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$";
+        return Pattern.matches(regex, email);}
+
+    // Méthodes getters pour accéder aux valeurs des attributs
     public String getNom() { return nom;}
     public String getPrenom() { return prenom;}
     public String getTelephone() { return telephone;}
     public String getEmail() { return email;}
     public String getCategorie() { return categorie;}
 
-  // Méthode pour retourner le nom complet
+    // Méthode pour retourner le nom complet
     public String getNomComplet() {
         return prenom + " " + nom; }
 
-  // Redéfinition de la méthode toString pour afficher les informations du contact
+    // Redéfinition de la méthode toString pour afficher les informations du contact
     @Override
     public String toString() {
         return "Nom complet : " + getNomComplet() +
